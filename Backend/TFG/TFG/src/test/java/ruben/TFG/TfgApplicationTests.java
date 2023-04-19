7fe611baefcab9fd2d7f340017546d9b11937590
rubenminero <rubenminero12@gmail.com>
2023-04-19 09:38:50 +0200

    added spring project

---
+++ Backend/TFG/TFG/src/test/java/ruben/TFG/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package ruben.TFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
